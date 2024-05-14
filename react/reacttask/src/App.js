7a5753e55689e5ef6f54f35aba077f00161471a1
GauravGoswami12967 <gaurav12967@gmail.com>
2024-05-14 11:55:33 +0530

    fisrt commit

--- react/reacttask/src/App.js
+++ react/reacttask/src/App.js
@@ -1,25 +1,33 @@
-import Css1 from "./CSS/Css1"
-import Css2 from "./CSS/Css2"
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+
+//import Css1 from "./CSS/Css1"
+//import Css2 from "./CSS/Css2"
 import Class_component from "./Component/Class_component"
 import Func_component from "./Component/Func_component"
 import Header from "./Header"
-import Jsx from "./JSX/Jsx"
+//import Jsx from "./JSX/Jsx"
 import Main_blog from "./Layout/Main_blog"
 import Main_contact from "./Layout/Main_contact"
 import Main_home from "./Layout/Main_home"
 import Class_Lifecycle from "./LifeCycle/Class/Class_Lifecycle"
 import Func_Lifecycle from "./LifeCycle/Func/Func_Lifecycle"
-import MUI from "./MUI/MUI"
-import Module_css from "./Module_css/Module_css"
+//import MUI from "./MUI/MUI"
+//import Module_css from "./Module_css/Module_css"
 import AMG from "./Props/Class/AMG"
 import BMS from "./Props/Function/BMS"
-import React_boostrap from "./React_boostrap/React_boostrap"
+//import React_boostrap from "./React_boostrap/React_boostrap"
 import Simple_bootstrap from "./React_boostrap/Simple_bootstrap"
-import Sass_css from "./Sass_css/Sass_css"
+//import Sass_css from "./Sass_css/Sass_css"
 import Class_state from "./State/Class/Class_state"
 import Func_state from "./State/function/Func_state"
-import Styled_component from "./Styled_component/Styled_component"
-import Tailwind from "./Tailwind/Tailwind"
+//import Styled_component from "./Styled_component/Styled_component"
+//import Tailwind from "./Tailwind/Tailwind"
+import Home from "./Routing/Pages/Home"
+import About from "./Routing/Pages/About"
+import Contact from "./Routing/Pages/Contact"
+
+
+
 
 function App() {
   return (
@@ -68,11 +76,21 @@ function App() {
             //lifecycle
 
             //<Class_Lifecycle/>
-            <Func_Lifecycle/>
+            //<Func_Lifecycle/>
+
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<><Home/> </>}></Route>
+                <Route path="/about" element={<><About/> </>}></Route>
+                <Route path="/contact" element={<><Contact/> </>}></Route>
+              </Routes>
+            </BrowserRouter>
+
+
         }
 
         
-    
+        
     </div>
   )
 }
